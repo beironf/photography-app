9c752f435c4ce488a20acab30b46b135c484a08a
Fredrik Beiron <fredrik.beiron@annotell.com>
2019-11-02 01:56:58 +0100

    added a map to choose coordinates

--- client/src/PhotoEditor/PhotoForm.tsx
+++ client/src/PhotoEditor/PhotoForm.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React, {useState, createRef} from "react";
 import { Form, Button, Image, Row, Col } from "react-bootstrap";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+
 import { ImageUploader } from "./ImageUploader";
 import { PhotoSelector } from "./PhotoSelector";
 import { getExif, PhotoExif } from "./getExif";
@@ -8,7 +10,6 @@ import { getExif, PhotoExif } from "./getExif";
 import { Photo } from "../model/photo";
 import { PhotoCategory, CameraTechnique } from "../model/metadata";
 import { Camera, Lens } from "../model/camera";
-
 
 const PATH_TO_GET_IMAGE = "http://localhost:3001/api/images?filename=";
 const PATH_TO_GET_THUMBNAIL = "http://localhost:3001/api/thumbnails?filename=";
@@ -32,6 +33,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
   const [imageFilename, setImageFilename] = useState<string>("IMG_4459.jpg");
   const [photo, setPhoto] = useState<Photo>();
   const [exif, setExif] = useState<PhotoExif>(initialExif);
+  const [mapLatLng, setMapLatLng] = useState<number[]>();
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -57,6 +59,10 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
     getExif(event.currentTarget, setExif);
   };
 
+  const handleMapClick = (event: any) => {
+    setMapLatLng([event.latlng.lat, event.latlng.lng]);
+  };
+
   if (imageFilename === undefined) {
     return (
       <div>
@@ -68,7 +74,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
 
   return (
     <div>
-      <Image style={{height: 600}} id="image" src={PATH_TO_GET_IMAGE + imageFilename} onLoad={loadExif}/>
+      <Image style={{height: 200}} id="image" src={PATH_TO_GET_IMAGE + imageFilename} onLoad={loadExif}/>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group as={Row}>
           <Form.Label column sm="2">Camera</Form.Label>
@@ -76,7 +82,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
             <Form.Control 
               as="select" 
               required
-              readOnly={exif.cameraGear.camera != null} 
+              disabled={exif.cameraGear.camera != null} 
             >
               {exif.cameraGear.camera != null && (
                 <option key={"camera-" + exif.cameraGear.camera}>
@@ -96,7 +102,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
             <Form.Control 
               as="select" 
               required
-              readOnly={exif.cameraGear.lens != null} 
+              disabled={exif.cameraGear.lens != null} 
             >
               {exif.cameraGear.lens != null && (
                 <option key={"lens-" + exif.cameraGear.lens}>
@@ -116,7 +122,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
             <Form.Control 
               type="text" 
               plaintext={exif.cameraSettings.exposureTime !== ""}
-              readOnly={exif.cameraSettings.exposureTime !== ""}
+              disabled={exif.cameraSettings.exposureTime !== ""}
               placeholder={exif.cameraSettings.exposureTime}
               required
             />
@@ -129,7 +135,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
             <Form.Control 
               type="text"
               plaintext={!isNaN(exif.cameraSettings.fNumber)}
-              readOnly={!isNaN(exif.cameraSettings.fNumber)}
+              disabled={!isNaN(exif.cameraSettings.fNumber)}
               placeholder={(!isNaN(exif.cameraSettings.fNumber)) ? exif.cameraSettings.fNumber.toString() : ""}
               required
             />
@@ -142,7 +148,7 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
             <Form.Control 
               type="text"
               plaintext={!isNaN(exif.cameraSettings.iso)}
-              readOnly={!isNaN(exif.cameraSettings.iso)}
+              disabled={!isNaN(exif.cameraSettings.iso)}
               placeholder={(!isNaN(exif.cameraSettings.iso)) ? exif.cameraSettings.iso.toString() : ""}
               required
             />
@@ -170,10 +176,47 @@ export const PhotoForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
           })}
         </Form.Group>
 
+        <Form.Group>
+          <Form.Label>Location</Form.Label>
+          <Form.Control type="text" placeholder="Name of Location" required />
+        </Form.Group>
+
+        <Form.Group as={Row}>
+          <Form.Label column sm="2">Coordinates</Form.Label>
+          <Col sm="10">
+            <Form.Control 
+              type="text" 
+              plaintext
+              disabled
+              placeholder={(mapLatLng) 
+                ? "Latitude: " + Math.round(mapLatLng[0] * 1000) / 1000 + 
+                  "  Longitude: " + Math.round(mapLatLng[1] * 1000) / 1000 
+                : "Click on the map to choose"
+              }
+              required
+            />
+          </Col>
+        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
+
+      <Map
+        style={{height: 500}}
+        center={[50, 10]}
+        zoom={3}
+        onclick={handleMapClick}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        {mapLatLng && (
+        <Marker position={mapLatLng as LatLngExpression}/>
+        )}
+      </Map>
     </div>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,5 +4,20 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
+// Fixes for leaflet package
+import "leaflet/dist/leaflet.css";
+import L, { LatLngExpression } from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconAnchor: [12.5, 41],
+});
+L.Popup.prototype.options.offset = L.point(1, -18);
+L.Marker.prototype.options.icon = DefaultIcon;
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
