f21e3d3a953c98907ef31e601112914fe70e9132
Fredrik Beiron <fredrik.beiron@annotell.com>
2020-12-20 19:46:11 +0100

    Complete photo model

--- client/src/model/photo.ts
+++ client/src/model/photo.ts
@@ -2,14 +2,21 @@ import { Location } from "./location";
 import { CameraGear, CameraSettings } from "./camera";
 import { Metadata } from "./metadata";
 
+export type Judgement = {
+  rating: number;
+  inShowroom: boolean;
+};
+
 export type Photo = {
   imageId: string;
   photographer: string;
   title: string;
   description?: string;
+  group?: string;
   taken: any; // Timestamp
   location: Location;
   gear: CameraGear;
   cameraSettings: CameraSettings;
   metadata: Metadata;
+  judgement: Judgement;
 };
