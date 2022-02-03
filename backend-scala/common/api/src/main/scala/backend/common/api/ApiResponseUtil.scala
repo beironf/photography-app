3d12b14aa4c0a6b99c4ed5dc34e0f64dccf38a58
Fredrik Beiron <fredrik@beiron.se>
2022-02-03 11:39:03 +0100

    Now compiling :)

--- backend-scala/common/api/src/main/scala/backend/common/api/ApiResponseUtil.scala
+++ backend-scala/common/api/src/main/scala/backend/common/api/ApiResponseUtil.scala
@@ -3,6 +3,8 @@ package backend.common.api
 import akka.http.scaladsl.model.StatusCode
 import akka.http.scaladsl.server.StandardRoute
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
+import backend.common.api.model.ResponseMessage
+import backend.common.api.model.ResponseMessageJsonProtocol._
 import spray.json._
 
 object ApiResponseUtil {
@@ -10,16 +12,12 @@ object ApiResponseUtil {
   import ApiJsonSupport._
 
   def completeWithErrorCode(statusCode: StatusCode, message: String): StandardRoute =
-    complete(statusCode -> Map("message" -> message.toJson))
+    complete(ResponseMessage(statusCode.intValue, Map("message" -> message.toJson)))
 
   def completeEnveloped[T: JsonFormat](statusCode: StatusCode, serializableClass: T): StandardRoute =
-    complete(statusCode -> Map("data" -> serializableClass.toJson))
+    complete(ResponseMessage(statusCode.intValue, Map("data" -> serializableClass.toJson)))
 
   def completeEnveloped[T: JsonFormat](statusCode: StatusCode, serializableClasses: Seq[T]): StandardRoute =
-    complete(statusCode -> Map("data" -> serializableClasses.toJson))
-
-  case class ResponseMessage(status: Long, message: String)
-
-  implicit def responseMessageFormat: RootJsonFormat[ResponseMessage] = jsonFormat2(ResponseMessage.apply)
+    complete(ResponseMessage(statusCode.intValue, Map("data" -> serializableClasses.toJson)))
 
 }
