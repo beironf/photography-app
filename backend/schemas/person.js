223b5c95c487e89e2b6b7c2d68ace380b6fa3d62
Fredrik Beiron <fredrik@beiron.se>
2022-05-14 11:02:15 +0200

    remove old node-backend

--- backend/schemas/person.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const PersonSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Person', PersonSchema);
