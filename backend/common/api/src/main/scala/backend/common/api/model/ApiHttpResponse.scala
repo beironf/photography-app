480bfdbebd491acb9cde10cb44cbf0ca50461795
Fredrik Beiron <fredrik@beiron.se>
2022-08-30 16:20:26 +0200

    update generic type to match tapir notation

--- backend/common/api/src/main/scala/backend/common/api/model/ApiHttpResponse.scala
+++ backend/common/api/src/main/scala/backend/common/api/model/ApiHttpResponse.scala
@@ -3,6 +3,6 @@ package backend.common.api.model
 import backend.common.api.model.ApiHttpErrors.HttpError
 
 object ApiHttpResponse {
-  type HttpResponse[R] = Either[HttpError, R]
-  type EnvelopedHttpResponse[R] = HttpResponse[Enveloped[R]]
+  type HttpResponse[O] = Either[HttpError, O]
+  type EnvelopedHttpResponse[O] = HttpResponse[Enveloped[O]]
 }
