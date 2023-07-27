2f7749dbd043fb5144d28afdf207ac9e3cdb68ee
Fredrik Beiron <fredrik@beiron.se>
2023-07-28 00:03:56 +0200

    adding prod config in frontend

--- frontend/src/Config.tsx
+++ frontend/src/Config.tsx
@@ -1,9 +1,24 @@
-type ToolsConfig = {
+type EnvironmentConfig = {
+  environment: string;
   photoApi: string;
   imageApi: string;
 };
 
-export const Config: ToolsConfig = {
-  photoApi: 'http://0.0.0.0:3001',
-  imageApi: 'http://0.0.0.0:3002',
+const DEVELOPMENT_CONFIG: EnvironmentConfig = {
+  environment: 'development',
+  photoApi: 'http://localhost:3001',
+  imageApi: 'http://localhost:3002',
 };
+
+const PRODUCTION_CONFIG: EnvironmentConfig = {
+  environment: 'production',
+  photoApi: 'http://photo-api:3001',
+  imageApi: 'http://image-api:3002',
+};
+
+const host = window.location.hostname;
+const isDevelopment = host.includes('localhost');
+
+export const Config: EnvironmentConfig = isDevelopment
+  ? DEVELOPMENT_CONFIG
+  : PRODUCTION_CONFIG;
