d11d4219f8493a853484b4c2607471f59fbb3011
Fredrik Beiron <fredrik@beiron.se>
2023-07-28 13:27:01 +0200

    updating frontend api urls

--- frontend/src/Config.tsx
+++ frontend/src/Config.tsx
@@ -12,8 +12,8 @@ const DEVELOPMENT_CONFIG: EnvironmentConfig = {
 
 const PRODUCTION_CONFIG: EnvironmentConfig = {
   environment: 'production',
-  photoApi: 'http://photo-api.default.svc.cluster.local:3001',
-  imageApi: 'http://image-api.default.svc.cluster.local:3002',
+  photoApi: 'http://photography.beiron.se/photo-api:3001',
+  imageApi: 'http://photography.beiron.se/image-api:3002',
 };
 
 const host = window.location.hostname;
