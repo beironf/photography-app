5f680c996cbe33a3942aab247f838a8b9c4422c7
Fredrik Beiron <fredrik@beiron.se>
2023-08-13 22:00:06 +0200

    Sync canon lenses

--- backend/photo/api/src/main/scala/backend/photo/api/model/enums/LensDto.scala
+++ backend/photo/api/src/main/scala/backend/photo/api/model/enums/LensDto.scala
@@ -6,8 +6,8 @@ sealed abstract class LensDto(override val name: String) extends TapirEnumEntry
 
 object LensDto extends TapirEnum[LensDto] {
   case object Sigma10_20 extends LensDto("Sigma 10-20mm f/4-5.6 EX DC HSM")
-  case object Canon50F1_8 extends LensDto("Canon 50mm f:1.8")
-  case object Canon70_300 extends LensDto("Canon 70-300mm f:4-5.6")
+  case object Canon50F1_8 extends LensDto("EF50mm f/1.8 II")
+  case object Canon70_300 extends LensDto("EF70-300mm f/4-5.6 IS USM")
   case object Canon24_70L extends LensDto("EF24-70mm f/2.8L USM")
   case object Fujinon35 extends LensDto("Fujinon 35mm f:2")
   case object DJI6_7F1_7 extends LensDto("DJI 6.7mm f/1.7")
