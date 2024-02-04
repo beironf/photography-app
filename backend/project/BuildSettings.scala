f4d824484321f305c6c21a05b45d18607d4ec862
Fredrik Beiron <fredrik@beiron.se>
2024-02-04 22:23:45 +0100

    add comment about heroku with git issue

--- backend/project/BuildSettings.scala
+++ backend/project/BuildSettings.scala
@@ -1,6 +1,7 @@
 import scala.language.postfixOps
 import scala.sys.process._
 
+// FIXME: Doesn't work with heroku - Can't find a git repository when deploying
 object BuildSettings {
   //lazy val gitBranch: String = ("git rev-parse --abbrev-ref HEAD" !!).trim
   //lazy val gitHash: String = ("git rev-parse HEAD" !!).trim
